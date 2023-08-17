a5960613423db2beaf8149a7a2702206e6ad088c
Imroj Syed <Imroj.Syed@gmail.com>
2023-08-17 04:30:04 -0500

    first commit

---
+++ lib/components/buttons.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class NameButton extends StatelessWidget {
+  const NameButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class SaveButton extends StatefulWidget {
+  const SaveButton({Key? key}) : super(key: key);
+
+  @override
+  State<SaveButton> createState() => SaveButtonState();
+}
+
+class SaveButtonState extends State<SaveButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
